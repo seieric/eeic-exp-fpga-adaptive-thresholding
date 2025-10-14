89a3fd932c54d572a26e803351e8134aab5090f1
seieric <30688855+seieric@users.noreply.github.com>
2025-10-14 15:07:01 +0900

    fix: 14bitアドレスを利用する

--- input_rom.v
+++ input_rom.v
@@ -1,9 +1,9 @@
 module input_rom (clock, address, q);
    input clock;
-   input [7:0] address; // 8bit address
+   input [13:0] address; // 14bit address
    output [7:0] q;  // 8bit
 
-   reg [7:0]    m[0:16383];
+   reg [7:0] m[0:16383];
 
    initial begin
       $readmemh("data/red-blood-cells_128x128.hex", m);
