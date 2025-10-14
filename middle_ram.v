ec44db333e1dab65a2abf26178eb5143a4a64861
seieric <30688855+seieric@users.noreply.github.com>
2025-10-14 16:21:53 +0900

    add: middle_ram, tb_middle_ram

---
+++ middle_ram.v
@@ -0,0 +1,16 @@
+// Box Filterの結果を保持するためのRAM
+module middle_ram (clock, data, wraddress, wren, rdaddress, q);
+   input clock, wren;
+   input [13:0] wraddress, rdaddress;
+   input [7:0]  data;
+   output [7:0] q;
+
+   reg [7:0]    m[0:16383];
+
+   assign q=m[rdaddress];
+   always @(posedge clock) begin
+      if( wren == 1) begin
+         m[wraddress]<=data;
+      end
+   end
+endmodule
---
+++ test/tb_middle_ram.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+
+module tb_middle_ram;
+    reg clk, wren;
+    reg [13:0] addr;
+    reg [7:0] wData;
+    wire [7:0] rData;
+
+    middle_ram middle_ram0 (
+        .clock(clk),
+        .rdaddress(addr),
+        .wraddress(addr),
+        .wren(wren),
+        .data(wData),
+        .q(rData)
+    );
+
+    initial clk = 0;
+    always #5 clk = ~clk; // 10ns周期
+
+    initial begin
+        wren = 1;
+        $display("Writing to RAM");
+        addr = 0;
+        repeat(50) begin
+            wData = addr; // 書き込むデータはアドレスと同じ値
+            $display("addr=%d, wData=%h", addr, wData);
+            #10;  // クロック1サイクル待機
+            addr = addr + 1;
+        end
+        #10;
+
+        wren = 0;
+        $display("Reading from RAM");
+        addr = 0;
+        repeat(50) begin
+            $display("addr=%d, rData=%h", addr, rData);
+            #10;  // クロック1サイクル待機
+            addr = addr + 1;
+        end
+        
+        #10;
+        $finish;
+    end
+
+endmodule
