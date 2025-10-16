8b2e11e2e39d661ebdc51b8d407ce68af96d8cef
seieric <30688855+seieric@users.noreply.github.com>
2025-10-16 14:51:10 +0900

    fix: input_rom_reader - 座標のビット数を修正

--- input_rom_reader.v
+++ input_rom_reader.v
@@ -1,19 +1,19 @@
 // メモリからデータを読み込むモジュール
 module input_rom_reader(clock, iCol, iRow, oData);
-    parameter WIDTH = 128;
-    parameter HEIGHT = 128;
+    parameter WIDTH_BITS = 7; // width=128
+    parameter HEIGHT_BITS = 7; // height=128
     parameter ADDR_WIDTH = 14; // log2(WIDTH * HEIGHT)
 
     input wire clock;
-    input wire [7:0] iCol; // 画像のX座標
-    input wire [7:0] iRow; // 画像のY座標
+    input wire [WIDTH_BITS-1:0] iCol; // 画像のX座標
+    input wire [HEIGHT_BITS-1:0] iRow; // 画像のY座標
     output reg [7:0] oData; // ピクセル値
 
     wire [ADDR_WIDTH-1:0] address;
     wire [7:0] mem_data;
 
     // 座標からメモリアドレスを計算
-    assign address = iRow * WIDTH + iCol;
+    assign address = (iRow << WIDTH_BITS) + iCol;
 
     // メモリからデータを読み込む
     input_rom rom (
